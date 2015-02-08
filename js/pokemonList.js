ff01ac09e9887c6f3e408d2e1382a5cabdeff28d
gwcmk <gregwcmkim@gmail.com>
2015-02-07 21:24:17 -0600

    Added error checking

--- js/pokemonList.js
+++ js/pokemonList.js
@@ -1,16 +1,16 @@
 $( "form" ).submit(function( event ) {
 	event.preventDefault();
 	var listValues = $('textarea').val();
-	var list = listValues.split('\n')
+	var list = listValues.split('\n');
+	$('.output').empty();
 	for(var i = 0; i < list.length; i++){
-		$('.output').append( list[i] + 
+		if(list[i].match(/^\d{4}$/)){
+			$('.output').append( list[i] + 
 							 ' - <a href="' +
 							 'https://www.reddit.com/r/SVExchange/search?q=title%3A' + 
 							 list[i] + 
-							 '+flair%3A%27trainer+shiny+value%27&sort=relevance&restrict_sr=on&t=year">' + 
+							 '+flair%3A%27trainer+shiny+value%27&sort=relevance&restrict_sr=on&t=year" target="_blank">' + 
 							 'Link</a><br>');
+		}
 	}
-
-	
-	
 });
